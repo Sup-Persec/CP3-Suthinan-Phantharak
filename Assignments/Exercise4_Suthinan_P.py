8c810de59de06d9892914aa073ec89843f04771d
supremelyn <64889316+supremelyn@users.noreply.github.com>
2020-05-11 13:17:27 +0700

    Add files via upload

---
+++ Assignments/Exercise4_Suthinan_P.py
@@ -0,0 +1,10 @@
+Foun_Eng = 83.0
+Gen_Bus = 79.8
+In_To_Com_Sys = 87.2
+Com_Pro = 80.4
+print("-------------Your Score----------------")
+print("Foundation English               :",Foun_Eng)
+print("General Business                 :",Gen_Bus)
+print("Introduction to Computer Systems :",In_To_Com_Sys)
+print("Computer Programming             :",Com_Pro)
+print("---------------------------------------")
