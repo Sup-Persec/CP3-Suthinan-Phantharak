637e31ccf8981beb4fa45defc316045195a2bc2f
supremelyn <whiteseapp@gmail.com>
2020-05-20 12:22:00 +0700

    Lecture50

---
+++ Assignments/Lecture50_Suthinan_P.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    print(x+y)
+def sub(x,y):
+    print(x-y)
+def multi(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+first = int(input("Enter number >> "))
+sec = int(input("Enter number >> "))
+add(first,sec)
+sub(first,sec)
+multi(first,sec)
+divide(first,sec)
