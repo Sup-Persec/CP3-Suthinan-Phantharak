47e5918484385e720ecaaf7a7bf01c11df7d4a74
Sup-Persec <64889316+Sup-Persec@users.noreply.github.com>
2021-01-24 17:11:29 +0700

    Add files via upload

---
+++ Assignments/Exercise21_Suthinan_P.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import math
+
+def leftclick(event):
+    try:
+        bmi = round(float(textBoxWeight.get())/math.pow(float(textBoxHight.get())/100,2),2)
+    except ValueError:
+        labelResult.configure(text="โปรดกรอกข้อมูลใหม่")
+
+    if bmi <= 18.5:
+        bmiResult = "ผอมเกินไป"
+    elif bmi <= 18.6 and bmi <= 22.9:
+        bmiResult = "น้ำหนักปกติ"
+    elif bmi <= 23 and bmi <= 24.9:
+        bmiResult = "น้ำหนักเกิน"
+    elif bmi <= 25 and bmi <= 29.9:
+        bmiResult = "อ้วน"
+    elif bmi >= 30:
+        bmiResult = "อ้วนมาก"
+
+    labelResult.configure(text="ผลประเมิน : " + bmiResult)
+
+MainWindow = Tk()
+
+labelHight = Label(MainWindow,text="ส่วนสูง(cm.)")
+labelHight.grid(row=0,column=0)
+textBoxHight = Entry(MainWindow)
+textBoxHight.grid(row=0,column=1)
+
+labelWeight = Label(MainWindow,text="น้ำหนัก(kg.)")
+labelWeight.grid(row=1,column=0)
+textBoxWeight = Entry(MainWindow)
+textBoxWeight.grid(row=1,column=1)
+
+calculateButton = Button(MainWindow,text="คำนวณ")
+calculateButton.bind('<Button-1>',leftclick)
+calculateButton.grid(row=2,column=0)
+
+labelResult = Label(MainWindow,text="ผลลัพธ์")
+labelResult.grid(row=2,column=1)
+
+MainWindow.mainloop()
